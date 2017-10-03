e3702edd8b1761854dc5f60d065f288b97e7e1bd
Paul Semel <paul.semel@lse.epita.fr>
2017-10-03 22:58:19 +0200

    update post comment form display
    posts/details.html: added some fancy bootstrap features to the form
    display
    
    posts/forms.py: added bootstrap attributes to form fields
    
    Signed-off-by: Paul Semel <paul.semel@lse.epita.fr>

--- app/posts/forms.py
+++ app/posts/forms.py
@@ -2,6 +2,9 @@ from django import forms
 from posts.models import Comment
 
 class CommentForm(forms.ModelForm):
+    content = forms.CharField(widget=forms.Textarea(attrs={'class':'form-control', 'placeholder': 'Your comment'}))
+    author = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Your nickname'}))
+    email = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Your email (won\'t appear)'}))
     class Meta:
         model = Comment
         fields = ('author', 'email', 'content')
