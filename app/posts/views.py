dbb4a898e43b0b24f45dded11f9264eb5d018f80
Paul Semel <paul.semel@lse.epita.fr>
2017-10-03 11:52:20 +0200

    add PostsDetails view and template
    views.py: this class finds the correct post according to the slug
    given, and displays the content of the posts in the detail.html template
    
    Signed-off-by: Paul Semel <paul.semel@lse.epita.fr>

--- app/posts/views.py
+++ app/posts/views.py
@@ -1,3 +1,5 @@
+import os
+import markdown
 from templates.myposts import meta
 from django.shortcuts import render
 from django.views.generic import TemplateView
@@ -21,3 +23,29 @@ class PostList(TemplateView):
         return render(request, self.template_name, locals())
 
 # Create your views here.
+
+class PostDetail(TemplateView):
+    template_name = "posts/detail.html"
+    def get(self, request, slug):
+        posts = get_posts()
+        post = None
+        for p in posts:
+            if (slugify(p['title']) == slug):
+                post = p
+        if post is None:
+            return render(request, "404.html")
+        title = post['title']
+        f = open(os.path.join(os.path.dirname(meta.__file__), post['filename']), 'r')
+        content = f.read()
+        post['html'] = markdown.markdown(content, ['codehilite', 'markdown.extensions.extra'])
+        del posts, f, content
+        return render(request, self.template_name, locals())
+    def post(self, request, slug):
+        posts = get_posts()
+        post = None
+        for p in posts:
+            if (slugify(p['title']) == slug):
+                post = p
+        if post is None:
+            return render(request, "404.html")
+        return render(request, self.template_name, locals())
