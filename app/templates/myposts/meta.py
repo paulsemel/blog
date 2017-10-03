6b96bfaeb5e805edcbd7aa50cf629dd039eaf50a
Paul Semel <paul.semel@lse.epita.fr>
2017-10-03 11:34:54 +0200

    add structure for markdown posts
    meta.py: this file is giving information about the different posts.md
    files we have in the director
    
    test.md: this is just a basic test file
    
    Signed-off-by: Paul Semel <paul.semel@lse.epita.fr>

---
+++ app/templates/myposts/meta.py
@@ -0,0 +1,29 @@
+# Status :
+#   - 0 : disabled
+#   - 1 : enabled
+
+# Be careful, comments are linked with posts with the id field. If those are
+# changed, comments will be losts or even worst exchanged.
+DISABLED = 0
+ENABLED = 1
+POSTS_META = [
+        {
+            'id': 0,
+            'filename': 'test.md',
+            'title': 'Post test',
+            'author': 'Paul Semel',
+            'date': '02-10-2017',
+            'short': 'This post is a test post dealing with a lot of different tests, read it, you wont be disappointed',
+            'status': ENABLED
+        },
+        {
+            'id': 1,
+            'filename': 'valgrind.md',
+            'title': 'Detecting bad stack usage using Valgrind Core API',
+            'author': 'Paul Semel',
+            'date': '02-10-2017',
+            'short': 'In this post, we will see what strategies we must employ to \
+                      be able to detect bad stack usage such as buffer overflows.',
+            'status': ENABLED
+        },
+]
