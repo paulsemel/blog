ccc914e274e8e1a66152f4af1bbde1a0578e2b6a
Paul Semel <paul.semel@lse.epita.fr>
2017-10-03 22:39:09 +0200

    add basic view for about section
    Signed-off-by: Paul Semel <paul.semel@lse.epita.fr>

--- app/about/views.py
+++ app/about/views.py
@@ -1,2 +1,10 @@
+import os
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
+class AboutView(TemplateView):
+    template_name = "about/index.html"
+    def get(self, request):
+        return render(request, self.template_name, locals())
+
 # Create your views here.
