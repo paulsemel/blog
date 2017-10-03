e600bea605eec75f46944a4f57096cbe410b9e3f
Paul Semel <paul.semel@lse.epita.fr>
2017-10-03 11:57:12 +0200

    add Comment model to posts app
    posts/models.py: the Comment model will permit the user to leave
    comments on each posts of this blog
    
    Signed-off-by: Paul Semel <paul.semel@lse.epita.fr>

--- app/posts/models.py
+++ app/posts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+class Comment(models.Model):
+    post_id = models.IntegerField()
+    content = models.CharField(max_length=500)
+    author = models.CharField(max_length=50)
+    email = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+
 # Create your models here.
