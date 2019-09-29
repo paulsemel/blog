da87843e478543df56571467e2e271ff3ef8cf0c
Paul Semel <semelpaul@gmail.com>
2019-09-29 12:31:29 -0400

    gatsby: changed gatsby-remark-images config
    Signed-off-by: Paul Semel <semelpaul@gmail.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,8 @@ module.exports = {
           {
             resolve: "gatsby-remark-images",
             options: {
-              maxWidth: 1035,
+              maxWidth: 1200,
+              linkImagesToOriginal: false,
             },
           },
           {
